b254917c2e9be16512a04be42698b350747f8b45
HimanshuAgrahari07 <himanshu.a@osmosys.asia>
2022-01-12 15:20:35 +0530

    Add try catch

--- src/app/controllers/handleResponse.js
+++ src/app/controllers/handleResponse.js
@@ -12,7 +12,7 @@ const _run = async (req, res, query) => {
 
         const hasRecords = queryDataFromDB && queryDataFromDB.length !== 0
         if (!hasRecords) return handleResponseStatus(req, res, { statusCode: 200, message: 'No records found' })
-        
+
         handleResponseStatus(req, res, { statusCode: 200, jsonDataToReturn: queryDataFromDB })
     } catch (err) {
         console.log(err)
@@ -21,52 +21,72 @@ const _run = async (req, res, query) => {
 }
 
 const getAllRows = async (req, res, next) => {
-    const query = `Select * from ${table}`;
-
-    await _run(req, res, query)
+    try {
+        const query = `Select * from ${table}`;
+        await _run(req, res, query)
+    } catch (error) {
+        handleResponseStatus(req, res, { message: error.message, statusCode: 400 })
+        next(error)
+    }
 }
 
 const getRowById = async (req, res, next) => {
-    const id = req.params.id
-    if (!id) throw new Error('No id passed')
-
-    const query = `Select * from ${table} where id = ${id}`
-    await _run(req, res, query)
+    try {
+        const id = req.params.id
+        const query = `Select * from ${table} where id = ${id}`
+        await _run(req, res, query)
+    } catch (error) {
+        handleResponseStatus(req, res, { message: error.message, statusCode: 400 })
+        next(error)
+    }
 }
 
 const insertRow = async (req, res, next) => {
-    const { name } = req.body
-    if (!name) handleResponseStatus(req, res, { statusCode: 400 })
-
-    const query = `INSERT INTO ${table} (name) VALUES ('${name}');`
-
-    await _run(req, res, query)
+    try {
+        const { name } = req.body
+        if (!name) handleResponseStatus(req, res, { statusCode: 400 })
+        const query = `INSERT INTO ${table} (name) VALUES ('${name}');`
+        await _run(req, res, query)
+    } catch (error) {
+        handleResponseStatus(req, res, { message: error.message, statusCode: 400 })
+        next(error)
+    }
 }
 
 const updateRow = async (req, res, next) => {
-    const id = req.params.id
-    const body = req.body;
-    const tableColumns = ['name', 'status', 'isDeleted']
+    try {
+        const id = req.params.id
+        const body = req.body;
+        const tableColumns = ['name', 'status', 'isDeleted']
 
-    const regex = tableColumns.join('|')
-    const requiredData = Object.entries(body).filter(e => e[0].match(regex))
-    const queryString = requiredData.map(e => `${e[0]}='${e[1]}'`).join(', ')
-    const query = `update ${table}
-    set ${queryString}
-    where id = ${id};`
+        const regex = tableColumns.join('|')
+        const requiredData = Object.entries(body).filter(e => e[0].match(regex))
+        const queryString = requiredData.map(e => `${e[0]}='${e[1]}'`).join(', ')
+        const query = `update ${table}
+        set ${queryString}
+        where id = ${id};`
 
-    await _run(req, res, query)
+        await _run(req, res, query)
+    } catch (error) {
+        handleResponseStatus(req, res, { message: error.message, statusCode: 400 })
+        next(error)
+    }
 }
 
 const deleteRow = async (req, res, next) => {
-    const id = req.params.id
-    if (!id) handleResponseStatus(req, res, { message: "Id is required", statusCode: 400 })
+    try {
+        const id = req.params.id
+        if (!id) handleResponseStatus(req, res, { message: "Id is required", statusCode: 400 })
 
-    const query = `update ${table}
-    set isDeleted = '1'
-    where id = ${id};`
+        const query = `update ${table}
+        set isDeleted = '1'
+        where id = ${id};`
 
-    await _run(req, res, query)
+        await _run(req, res, query)
+    } catch (error) {
+        handleResponseStatus(req, res, { message: error.message, statusCode: 400 })
+        next(error)
+    }
 }
 
 module.exports = { getAllRows, getRowById, insertRow, updateRow, deleteRow }
